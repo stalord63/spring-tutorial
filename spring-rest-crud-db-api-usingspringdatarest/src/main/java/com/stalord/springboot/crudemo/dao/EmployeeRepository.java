10bde8b858c2c3880d69ed376e578c06ad89f2b3
Aditya Singh <adi06031998@gmail.com>
2024-10-13 03:00:50 +0530

    Add files via upload

---
+++ spring-rest-crud-db-api-usingspringdatarest/src/main/java/com/stalord/springboot/crudemo/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.stalord.springboot.crudemo.dao;
+
+import com.stalord.springboot.crudemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+
+
+}
---
+++ spring-rest-crud-db-api-usingspringdatarest/src/test/java/com/stalord/springboot/crudemo/CrudemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stalord.springboot.crudemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
